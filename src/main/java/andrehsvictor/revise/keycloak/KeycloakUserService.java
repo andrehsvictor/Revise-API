13f8b6ea44dcf25f1401f0a6d4a587e4df592f07
andrehsvictor <andrehsvictor@gmail.com>
2024-10-26 15:48:34 -0300

    Refactor KeycloakUserService to handle email verification and update user information

--- src/main/java/andrehsvictor/revise/keycloak/KeycloakUserService.java
+++ src/main/java/andrehsvictor/revise/keycloak/KeycloakUserService.java
@@ -6,8 +6,10 @@ import org.keycloak.admin.client.CreatedResponseUtil;
 import org.keycloak.admin.client.resource.RealmResource;
 import org.keycloak.admin.client.resource.UserResource;
 import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import andrehsvictor.revise.exception.ReviseException;
 import andrehsvictor.revise.user.User;
 import jakarta.ws.rs.core.Response;
 import lombok.RequiredArgsConstructor;
@@ -38,7 +40,12 @@ public class KeycloakUserService {
     }
 
     public void sendVerifyEmail(String id) {
-        findById(id).sendVerifyEmail();
+        UserResource userResource = findById(id);
+        UserRepresentation userRepresentation = userResource.toRepresentation();
+        if (userRepresentation.isEmailVerified()) {
+            throw new ReviseException(HttpStatus.BAD_REQUEST, List.of("E-mail is already verified"));
+        }
+        userResource.sendVerifyEmail();
     }
 
     public UserRepresentation update(User user, UserRepresentation userRepresentation) {
@@ -46,6 +53,10 @@ public class KeycloakUserService {
         userRepresentation.setLastName(user.getLastName() != null ? user.getLastName() : userRepresentation.getLastName());
         userRepresentation.setEmail(user.getEmail() != null ? user.getEmail() : userRepresentation.getEmail());
         userRepresentation.setUsername(user.getUsername() != null ? user.getUsername() : userRepresentation.getUsername());
+
+        if (user.getEmail() != null && !user.getEmail().equals(userRepresentation.getEmail())) {
+            userRepresentation.setEmailVerified(false);
+        }
 
         keycloak.users().get(userRepresentation.getId()).update(userRepresentation);
         return findById(userRepresentation.getId()).toRepresentation();
