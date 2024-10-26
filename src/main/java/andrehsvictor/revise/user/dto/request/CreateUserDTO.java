5eb070edec4501a8613f971c995a5def05504ff0
andrehsvictor <andrehsvictor@gmail.com>
2024-10-26 19:40:26 -0300

    Refactor DTO class names for user requests

--- src/main/java/andrehsvictor/revise/user/dto/request/CreateUserDTO.java
+++ src/main/java/andrehsvictor/revise/user/dto/request/CreateUserDTO.java
@@ -2,6 +2,7 @@ package andrehsvictor.revise.user.dto.request;
 
 import andrehsvictor.revise.user.User;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -9,20 +10,29 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class UpdateUserRequestDTO {
+public class CreateUserDTO {
+
+    @NotBlank(message = "First name is required")
     @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
     private String firstName;
 
+    @NotBlank(message = "Last name is required")
     @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
     private String lastName;
 
-    @Size(min = 2, max = 50, message = "Username must be between 2 and 50 characters")
+    @NotBlank(message = "E-mail is required")
     @Email(message = "E-mail must be valid")
     private String email;
 
+    @NotBlank(message = "Username is required")
     @Size(min = 2, max = 50, message = "Username must be between 2 and 50 characters")
     @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Username must contain only letters and numbers")
     private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 50, message = "Password must be between 6 and 50 characters")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message = "Password must contain at least one uppercase letter, one lowercase letter and one number")
+    private String password;
 
     private String avatarUrl;
 
--- src/main/java/andrehsvictor/revise/user/dto/request/UpdateUserDTO.java
+++ src/main/java/andrehsvictor/revise/user/dto/request/UpdateUserDTO.java
@@ -2,7 +2,6 @@ package andrehsvictor.revise.user.dto.request;
 
 import andrehsvictor.revise.user.User;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -10,29 +9,20 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class UserRequestDTO {
-
-    @NotBlank(message = "First name is required")
+public class UpdateUserDTO {
     @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
     private String firstName;
 
-    @NotBlank(message = "Last name is required")
     @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
     private String lastName;
 
-    @NotBlank(message = "E-mail is required")
+    @Size(min = 2, max = 50, message = "Username must be between 2 and 50 characters")
     @Email(message = "E-mail must be valid")
     private String email;
 
-    @NotBlank(message = "Username is required")
     @Size(min = 2, max = 50, message = "Username must be between 2 and 50 characters")
     @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Username must contain only letters and numbers")
     private String username;
-
-    @NotBlank(message = "Password is required")
-    @Size(min = 6, max = 50, message = "Password must be between 6 and 50 characters")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message = "Password must contain at least one uppercase letter, one lowercase letter and one number")
-    private String password;
 
     private String avatarUrl;
 
